81baf9f6bfc183e363cb3ce720f18510e56b0a12
Stu Smith <stusmith1977@gmail.com>
2013-12-16 21:16:12 +0000

    Extraction of values from SQL, conversion to JSON.

---
+++ src/Api/Resources.hs
@@ -0,0 +1,32 @@
+
+{-# LANGUAGE OverloadedStrings #-}
+
+module Api.Resources
+(
+    apiGetResources
+)
+where
+
+import Data.Aeson as J            ( Value, object, (.=) )
+import Data.Text                  ( Text )
+import Control.Monad.IO.Class     ( liftIO )
+import Web.Scotty                 ( ActionM, json )
+import Database.Esqueleto as Sql  ( select, from, (^.), Value(..) )
+
+import Schema
+import Database                ( runSql )
+
+
+apiGetResources :: ActionM ()
+apiGetResources
+    = json =<< liftIO sqlGetAllResourcesSummary
+
+sqlGetAllResourcesSummary :: IO [J.Value]
+sqlGetAllResourcesSummary
+    = runSql $ do rows <- select $ from $ \resource ->
+                          return $ resource ^. ResourcePath
+                  return $ map asJSON rows
+
+asJSON :: Sql.Value Text -> J.Value
+asJSON (Sql.Value path)
+    = object ["path" .= path]
